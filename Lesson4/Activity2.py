025f11b590f583e74145b462bb6495eca33c2918
Rvpx12332 <aarvixettigar@gmail.com>
2025-12-20 13:41:02 +0000

    lesson4.activity2.py

---
+++ Lesson4/Activity2.py
@@ -0,0 +1,8 @@
+science = float(input("Enter your science score:"))
+maths = float(input("Enter your maths score:"))
+english = float(input("Enter your english score:"))
+
+sum = science + maths + english
+percentage = sum/3
+
+print("Your total percentage is", percentage, "%")
