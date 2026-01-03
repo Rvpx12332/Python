e2e8dc6cd8ff269f3b94dea95137e30f764fc64b
Rvpx12332 <aarvixettigar@gmail.com>
2026-01-03 13:10:01 +0000

    Lesson6.Activity1.py

---
+++ Lesson6/Activity1.py
@@ -0,0 +1,10 @@
+x = int(input("Enter a number here:"))
+
+if x<= 10:
+    print(x, "is between 0-10")
+elif x<= 20:
+    print(x,"is between 10-20")
+elif x<= 30:
+    print(x,"is between 20-30")
+else:
+    print(x,"is higher than 30")    
