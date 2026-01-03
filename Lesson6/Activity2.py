5913a1565e462c2f0b7766eb9548d8ef7a366b10
Rvpx12332 <aarvixettigar@gmail.com>
2026-01-03 13:20:24 +0000

    Lesson6.Activity2.py

---
+++ Lesson6/Activity2.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age:"))
+citizen = input("Are you a citizen? (yes/no)")
+
+if age>= 18 and citizen == "yes":
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")    
