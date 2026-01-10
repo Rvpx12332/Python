97ebc766b5cbcc7aaa8a6040d1c5142e0e882186
Rvpx12332 <aarvixettigar@gmail.com>
2026-01-10 13:37:32 +0000

    Lesson7.Activity3.py

---
+++ Lesson 7/Activity3.py
@@ -0,0 +1,14 @@
+marks = int(input("Enter your marks:"))
+
+if marks <= 50:
+    print("F")
+elif marks <= 60:
+    print("D")
+elif marks <= 70:
+    print("C")    
+elif marks <= 80:
+    print("B") 
+elif marks <= 90:
+    print("A")       
+else:
+    print("A*")
