2417babca1334f3fe82f53498f776db4f5bd780a
Rvpx12332 <aarvixettigar@gmail.com>
2026-01-17 20:43:26 +0000

    Bday.py

---
+++ Hw/BDays.py
@@ -0,0 +1,21 @@
+name1 = input("Enter first friend's name: ")
+birthday1 = input("Enter birthday: ")
+
+name2 = input("Enter second friend's name: ")
+birthday2 = input("Enter birthday: ")
+
+name3 = input("Enter third friend's name: ")
+birthday3 = input("Enter birthday: ")
+
+name4 = input("Enter fourth friend's name: ")
+birthday4 = input("Enter birthday: ")
+
+name5 = input("Enter fifth friend's name: ")
+birthday5 = input("Enter birthday: ")
+
+print("Birthdays:")
+print(name1, "-", birthday1)
+print(name2, "-", birthday2)
+print(name3, "-", birthday3)
+print(name4, "-", birthday4)
+print(name5, "-", birthday5)
