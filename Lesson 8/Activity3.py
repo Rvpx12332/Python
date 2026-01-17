7bd3fabc13e13b723ad2d9b88ff3edb7e2965a28
Rvpx12332 <aarvixettigar@gmail.com>
2026-01-17 13:39:03 +0000

    Lesson8Activity3.py

---
+++ Lesson 8/Activity3.py
@@ -0,0 +1,10 @@
+incorrect_mean = 38
+wrong_num = 36
+correct_num = 56
+total_num = 30
+
+sum = total_num*incorrect_mean
+sum = sum-wrong_num+correct_num
+new_mean = sum/total_num
+
+print("The correct mean is", new_mean)
